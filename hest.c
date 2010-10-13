d36f88e9a45720b37511c44118d5b8ca1af605d9
Martin Stensgård <mastensg@ping.uio.no>
2010-10-13 18:40:44 +0200

    Removed trailing whitespace.

--- hest.c
+++ hest.c
@@ -41,7 +41,7 @@ static Window windows[40];
 
 static int keys[40] = {
     XK_1, XK_2, XK_3, XK_4, XK_5, XK_6, XK_7, XK_8,     XK_9,      XK_0,
-    XK_q, XK_w, XK_e, XK_r, XK_t, XK_y, XK_u, XK_i,     XK_o,      XK_p, 
+    XK_q, XK_w, XK_e, XK_r, XK_t, XK_y, XK_u, XK_i,     XK_o,      XK_p,
     XK_a, XK_s, XK_d, XK_f, XK_g, XK_h, XK_j, XK_k,     XK_l,      XK_semicolon,
     XK_z, XK_x, XK_c, XK_v, XK_b, XK_n, XK_m, XK_comma, XK_period, XK_slash
 };
